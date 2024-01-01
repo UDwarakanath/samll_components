070ab70b937b668dde0ee22bd6a4fcb10f8130e8
UDwarakanath <dwarakanathummadisetti@gmail.com>
2024-01-01 16:20:15 +0530

    accordian started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import CustomModal from "./Modal/CustomModal"
 import ModalContent from "./Modal/ModalContent"
+import Accordian from "./Accordian/Accordian"
 import "./App.css"
+
 const App = () => {
   const modal_content = <ModalContent/>
   const consoleFunc = ()=>{
     console.log("callback works perfectly")
   }
-  return <CustomModal content={modal_content} buttonSubmitText="update" cb={consoleFunc}/>
+  return (
+    <>
+    <CustomModal content={modal_content} buttonSubmitText="update" cb={consoleFunc}/>
+    <Accordian/>
+    </>
+  )
 }
 
 export default App
